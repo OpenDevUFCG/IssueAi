5b228237004664692214e4152f54445054b6469d
Thayanne <thayannevls@gmail.com>
2018-11-04 02:01:22 -0300

    add axios

--- src/lib/github.js
+++ src/lib/github.js
@@ -1,1 +1,12 @@
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+// @flow
+import axios from 'axios';
+
+const getAxiosInstance = () => {
+    const token = process.env.GITHUB_TOKEN || '';
+    const config = {
+        baseUrl: 'https://api.github.com/graphql',
+        headers: { Authorization: `Bearer ${token}` },
+    };
+
+    return axios.create(config);
+};
