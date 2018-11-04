e76718877c91b0c2b59eeec33f54326bef1d4cf2
Thayanne <thayannevls@gmail.com>
2018-11-04 02:24:14 -0300

    add query to get org repos

--- src/lib/github.js
+++ src/lib/github.js
@@ -1,7 +1,19 @@
 // @flow
 import axios from 'axios';
 
-const getAxiosInstance = () => {
+const queryGetRepos = `    
+    Organization(login: "OpenDevUFCG") {
+        repositories(first: 30) {
+            edges {
+                node {
+                nameWithOwner
+                }
+            }
+        }
+    }
+`;
+
+export const getAxiosInstance = () => {
     const token = process.env.GITHUB_TOKEN || '';
     const config = {
         baseUrl: 'https://api.github.com/graphql',
@@ -10,3 +22,15 @@ const getAxiosInstance = () => {
 
     return axios.create(config);
 };
+
+const requestGithub = async (query: string) => {
+    const params = {
+        params: { q: query, type: 'repository' },
+    };
+
+    const response = await getAxiosInstance().post('/', params);
+
+    return response.data;
+};
+
+export const getReposOrg = async () => requestGithub(queryGetRepos);
