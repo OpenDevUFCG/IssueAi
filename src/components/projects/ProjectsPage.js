58717accec2e40eec7db462fe8baca9b7c8f5825
Fanny <fannyvieira082@gmail.com>
2019-10-06 22:27:58 -0400

    Remove unused var

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -32,7 +32,7 @@ export default class ProjectsPage extends React.Component<void, AppState> {
     }
 
     updateRepositoryList = () => {
-        const { cursor, loading } = this.state;
+        const { cursor } = this.state;
         this.setState({ loading: true });
         getRepositories(cursor).then(({ repos, lastCursor }) => {
             this.setState(updateListState(repos, lastCursor));
