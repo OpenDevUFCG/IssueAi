d5a450231fb4ac6a7a06a3087ec3a326dd147bf1
Thayanne <thayannevls@gmail.com>
2019-10-11 17:01:03 -0300

    remove console.log

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import 'blueprint-css/dist/blueprint.min.css';
 
 const mountNode = document.getElementById('app');
-console.log(process.env.GITHUB_TOKEN);
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
