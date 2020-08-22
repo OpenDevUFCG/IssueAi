1ea45cae66ea4eecd7f74861f822a6dbb427ed1e
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2020-08-22 05:15:38 -0300

    Alterei o BrowserRouter para HashRouter em index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 import './index.css'
@@ -9,8 +9,8 @@ import 'blueprint-css/dist/blueprint.min.css'
 const mountNode = document.getElementById('app')
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   mountNode
 )
